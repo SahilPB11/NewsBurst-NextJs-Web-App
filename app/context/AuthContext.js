8da8872b8c9f750b71116b424c997748f39d35d4
Sahil <garg07825@gmail.com>
2023-12-05 20:48:47 +0530

    intstall firebase

---
+++ app/context/AuthContext.js
@@ -0,0 +1,83 @@
+"use client";
+import { useContext, createContext, useState, useEffect } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
+import { auth } from "../Firebase/firebaseConfig";
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const googleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user);
+      localStorage.setItem("user", JSON.stringify(result.user));
+    } catch (error) {
+      console.error("Error signing in with Google:", error.message);
+    }
+  };
+
+  const githubSignIn = async () => {
+    const provider = new GithubAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user);
+      localStorage.setItem("user", JSON.stringify(result.user));
+    } catch (error) {
+      console.error("Error signing in with GitHub:", error.message);
+    }
+  };
+
+  const emailPasswordSignUp = async (email, password) => {
+    try {
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      setUser(result.user);
+      localStorage.setItem("user", JSON.stringify(result.user));
+    } catch (error) {
+      console.error("Error signing up with email/password:", error.message);
+    }
+  };
+
+  const logOut = () => {
+    signOut(auth);
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, googleSignIn, githubSignIn, emailPasswordSignUp, logOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const UserAuth = () => {
+  return useContext(AuthContext);
+};
